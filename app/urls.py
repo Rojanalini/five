a43191b2e9ebfffc888df4c3681d66ed192cba28
Rojanalini <javvajirojanalini1234@gmail.com>
2023-11-21 20:41:40 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from app.views import *
+from django.urls import path
+app_name='virat'
+urlpatterns=[
+    path('msd/',msd,name='msd'),
+    
+]
---
+++ app2/urls.py
@@ -0,0 +1,7 @@
+from app2.views import *
+from django.urls import path
+app_name='anusha'
+urlpatterns=[
+    path('India/',India,name='India'),
+    
+]
